60e82e5ec6ebf0631e3fc7438392587ed4baa2fc
mallusrgreat <mallusrgreat@duck.com>
2026-01-20 13:01:38 +0530

    add some edge cases

--- src/main/java/me/mallusrgreat/neptunePlayerGlow/NeptunePlayerGlow.java
+++ src/main/java/me/mallusrgreat/neptunePlayerGlow/NeptunePlayerGlow.java
@@ -9,33 +9,43 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
 
 public final class NeptunePlayerGlow extends JavaPlugin implements Listener {
+    private Team red, blue;
+
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
-        getLogger().info("Enabled NeptunePlayerGlow");
+        Scoreboard scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
+
+        red = scoreboard.getTeam("Red") != null ? scoreboard.getTeam("Red") : scoreboard.registerNewTeam("Red");
+        blue = scoreboard.getTeam("Blue") != null ? scoreboard.getTeam("Blue") : scoreboard.registerNewTeam("Blue");
+
+        red.color(NamedTextColor.RED);
+        blue.color(NamedTextColor.BLUE);
     }
+
     @EventHandler
     public void onMatchStart(MatchStartEvent e) {
-        var match = e.getMatch();
-        var scoreboard = Bukkit.getScoreboardManager().getMainScoreboard();
-        Team red = scoreboard.registerNewTeam("Red");
-        red.color(NamedTextColor.RED);
-        Team blue = scoreboard.registerNewTeam("Blue");
-        blue.color(NamedTextColor.BLUE);
-        match.getParticipants().forEach(participant -> {
+        e.getMatch().getParticipants().forEach(participant -> {
             Player player = participant.getProfile().getPlayer();
-            if (participant.getColor().getContentColor().equals(Color.RED)) red.addPlayer(player);
-            else blue.addPlayer(player);
+            boolean isRed = participant.getColor().getContentColor().equals(Color.RED);
+
+            (isRed ? red : blue).addPlayer(player);
             player.setGlowing(true);
         });
     }
+
     @EventHandler
     public void onMatchEnd(MatchEndEvent e) {
-        e.getMatch().getParticipants().forEach(participant ->
-            participant.getProfile().getPlayer().setGlowing(false)
-        );
+        e.getMatch().getParticipants().forEach(participant -> {
+            Player player = Bukkit.getPlayer(participant.getPlayerUUID());
+            if (player == null) return;
+            player.setGlowing(false);
+            red.removePlayer(player);
+            blue.removePlayer(player);
+        });
     }
 }
